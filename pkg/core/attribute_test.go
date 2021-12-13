61e23297d8f46f681952fc8757e9f50a74a62ace
Johan Kristiansson <johan.kristiansson@protonmail.com>
2021-12-13 08:49:22 +0100

    added unit tests

--- pkg/core/attribute_test.go
+++ pkg/core/attribute_test.go
@@ -1,7 +1,25 @@
 package core
 
-import "testing"
+import (
+	. "colonies/pkg/utils"
+	"testing"
+)
 
 func TestCreateAttribute(t *testing.T) {
+	attribute := CreateAttribute(GenerateRandomID(), OUT, "test_key", "test_value")
+	if len(attribute.ID()) != 64 {
+		Fatal(t, "invalid attribute id length")
+	}
 
+	if attribute.AttributeType() != OUT {
+		Fatal(t, "invalid attribute type")
+	}
+
+	if attribute.Key() != "test_key" {
+		Fatal(t, "invalid attribute key")
+	}
+
+	if attribute.Value() != "test_value" {
+		Fatal(t, "invalid attribute value")
+	}
 }
