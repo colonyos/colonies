4f7036b72773873438926cfe9c36fe708cd3b142
Johan Kristiansson <johan.kristiansson@protonmail.com>
2022-02-08 23:09:46 +0100

    more files

--- deployment/k8s/install.sh
+++ deployment/k8s/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-namespace="$1"
+namespace="colonies"
+
+echo "-> Creating namespace" 
+kubectl apply -f namespace.yaml 
 
 echo "-> Creating TLS secret" 
 mkdir tls-cert
@@ -12,7 +15,7 @@ openssl genrsa -out colonies-cert-tls.key 2048
 openssl req -new -key colonies-cert-tls.key -subj "/CN=colonies-deployment.${namespace}.svc" \
     | openssl x509 -days 10000 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out colonies-cert-tls.crt
 
+cd ..
+
 echo "-> Creating Kubernetes TLS keys and secret"
-kubectl -n ${namespace} create secret tls colonies-tls \
-    --cert "./tls-cert/colonies-cert-tls.crt" \
-    --key "./tls-cert/colonies-cert-tls.key"
+kubectl -n ${namespace} create secret tls colonies-cert-tls --cert ./tls-cert/colonies-cert-tls.crt --key ./tls-cert/colonies-cert-tls.key
