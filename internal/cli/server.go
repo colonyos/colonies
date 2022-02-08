12b3daf3b2e5e0e55f4e4295a3817fb2ee629001
Johan Kristiansson <johan.kristiansson@protonmail.com>
2022-02-08 22:54:32 +0100

    support for k8s deployment

--- internal/cli/server.go
+++ internal/cli/server.go
@@ -15,22 +15,14 @@ func init() {
 	serverCmd.AddCommand(serverStartCmd)
 	rootCmd.AddCommand(serverCmd)
 
-	serverCmd.PersistentFlags().StringVarP(&DBHost, "dbhost", "", "localhost", "Colonies database host")
-	serverCmd.MarkPersistentFlagRequired("dbhost")
-	serverCmd.PersistentFlags().IntVarP(&DBPort, "dbport", "", 5432, "Colonies database port")
-	serverCmd.MarkPersistentFlagRequired("dbport")
+	serverCmd.PersistentFlags().StringVarP(&DBHost, "dbhost", "", "", "Colonies database host")
+	serverCmd.PersistentFlags().IntVarP(&DBPort, "dbport", "", DefaultDBPort, "Colonies database port")
 	serverCmd.PersistentFlags().StringVarP(&DBUser, "dbuser", "", "", "Colonies database user")
-	serverCmd.MarkPersistentFlagRequired("dbuser")
 	serverCmd.PersistentFlags().StringVarP(&DBPassword, "dbpassword", "", "", "Colonies database password")
-	serverCmd.MarkPersistentFlagRequired("dbpassword")
 	serverCmd.PersistentFlags().StringVarP(&TLSCert, "tlscert", "", "", "TLS certificate")
-	serverCmd.MarkPersistentFlagRequired("tlscert")
 	serverCmd.PersistentFlags().StringVarP(&TLSKey, "tlskey", "", "", "TLS key")
-	serverCmd.MarkPersistentFlagRequired("tlskey")
-	serverCmd.PersistentFlags().IntVarP(&ServerPort, "port", "", 8080, "Server HTTP port")
-	serverCmd.MarkPersistentFlagRequired("port")
+	serverCmd.PersistentFlags().IntVarP(&ServerPort, "port", "", DefaultServerPort, "Server HTTP port")
 	serverCmd.PersistentFlags().StringVarP(&ServerID, "serverid", "", "", "Colonies server Id")
-	serverCmd.MarkPersistentFlagRequired("serverid")
 }
 
 var serverCmd = &cobra.Command{
@@ -44,8 +36,67 @@ var serverStartCmd = &cobra.Command{
 	Short: "Start a Colonies server",
 	Long:  "Start a Colonies server",
 	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		if DBHost == "" {
+			DBHost = os.Getenv("DBHOST")
+		}
+
+		if DBPort != DefaultDBPort {
+			DBPortStr := os.Getenv("DBPORT")
+			DBPort, err = strconv.Atoi(DBPortStr)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(-1)
+			}
+		}
+
+		if DBUser == "" {
+			DBUser = os.Getenv("DBUSER")
+		}
+
+		if DBPassword == "" {
+			DBPassword = os.Getenv("DBPASSWORD")
+		}
+
+		if ServerPort != DefaultServerPort {
+			ServerPortStr := os.Getenv("SERVERPORT")
+			ServerPort, err = strconv.Atoi(ServerPortStr)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(-1)
+			}
+		}
+
+		if ServerID == "" {
+			ServerID = os.Getenv("SERVERID")
+		}
+
+		if TLSKey == "" {
+			TLSKey = os.Getenv("TLSKEY")
+		}
+
+		if TLSCert == "" {
+			TLSCert = os.Getenv("TLSCERT")
+		}
+
+		VerboseEnv := os.Getenv("VERBOSE")
+		if VerboseEnv == "true" {
+			Verbose = true
+		} else if VerboseEnv == "false" {
+			Verbose = false
+		}
+
+		fmt.Println("----------")
+		fmt.Println("DBHOST: ", DBHost)
+		fmt.Println("DBPORT: ", DBPort)
+		fmt.Println("DBUser: ", DBUser)
+		fmt.Println("DBPassword: ", DBPassword)
+		fmt.Println("DBName:", DBName)
+		fmt.Println("DBPrefix:", DBPrefix)
+		fmt.Println("----------")
+
 		db := postgresql.CreatePQDatabase(DBHost, DBPort, DBUser, DBPassword, DBName, DBPrefix)
-		err := db.Connect()
+		err = db.Connect()
 		if err != nil {
 			fmt.Println("Failed to connect to database")
 			os.Exit(-1)
