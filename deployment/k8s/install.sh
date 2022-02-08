2b5cdebf6877ea8c4e9f21a4207317e45a4e646f
Johan Kristiansson <johan.kristiansson@protonmail.com>
2022-02-08 22:54:32 +0100

    support for k8s deployment

---
+++ deployment/k8s/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+namespace="$1"
+
+echo "-> Creating TLS secret" 
+mkdir tls-cert
+chmod 0700 "./tls-cert"
+cd "./tls-cert"
+
+openssl req -nodes -new -x509 -days 10000 -keyout ca.key -out ca.crt -subj "/CN=ColonyOS Colonies CA"
+openssl genrsa -out colonies-cert-tls.key 2048
+openssl req -new -key colonies-cert-tls.key -subj "/CN=colonies-deployment.${namespace}.svc" \
+    | openssl x509 -days 10000 -req -CA ca.crt -CAkey ca.key -CAcreateserial -out colonies-cert-tls.crt
+
+echo "-> Creating Kubernetes TLS keys and secret"
+kubectl -n ${namespace} create secret tls colonies-tls \
+    --cert "./tls-cert/colonies-cert-tls.crt" \
+    --key "./tls-cert/colonies-cert-tls.key"
