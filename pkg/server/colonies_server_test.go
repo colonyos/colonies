2d64f531714db85777774d6eb1947f78056f7f7b
Johan Kristiansson <kristiansson.johan@gmail.se>
2022-04-08 08:31:36 +0200

    unit testing debugging and always exit 0 when creating db

--- pkg/server/colonies_server_test.go
+++ pkg/server/colonies_server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -152,6 +153,10 @@ func TestGetRuntimes(t *testing.T) {
 
 	runtimesFromServer, err := client.GetRuntimes(colony.ID, runtime1PrvKey)
 	assert.Nil(t, err)
+
+	fmt.Println(runtimes)
+	fmt.Println(runtimesFromServer)
+
 	assert.True(t, core.IsRuntimeArraysEqual(runtimes, runtimesFromServer))
 
 	server.Shutdown()
