3226ca789aefc05e020678ddbe7df2044d988942
Johan Kristiansson <kristiansson.johan@gmail.se>
2022-04-17 22:35:15 +0200

    improved tests

--- pkg/rpc/approve_runtime_msg_test.go
+++ pkg/rpc/approve_runtime_msg_test.go
@@ -1,1 +1,36 @@
 package rpc
+
+import (
+	"testing"
+
+	"github.com/colonyos/colonies/pkg/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRPCApproveRuntimeMsg(t *testing.T) {
+	msg := CreateApproveRuntimeMsg(core.GenerateRandomID())
+	jsonString, err := msg.ToJSON()
+	assert.Nil(t, err)
+
+	msg2, err := CreateApproveRuntimeMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateApproveRuntimeMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
+
+func TestRPCApproveRuntimeMsgIndent(t *testing.T) {
+	msg := CreateApproveRuntimeMsg(core.GenerateRandomID())
+	jsonString, err := msg.ToJSONIndent()
+	assert.Nil(t, err)
+
+	msg2, err := CreateApproveRuntimeMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateApproveRuntimeMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
---
+++ pkg/rpc/assign_process_msg_test.go
@@ -0,0 +1,36 @@
+package rpc
+
+import (
+	"testing"
+
+	"github.com/colonyos/colonies/pkg/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRPCAssignProcessMsg(t *testing.T) {
+	msg := CreateAssignProcessMsg(core.GenerateRandomID())
+	jsonString, err := msg.ToJSON()
+	assert.Nil(t, err)
+
+	msg2, err := CreateAssignProcessMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateAssignProcessMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
+
+func TestRPCAssignProcessMsgIndent(t *testing.T) {
+	msg := CreateAssignProcessMsg(core.GenerateRandomID())
+	jsonString, err := msg.ToJSONIndent()
+	assert.Nil(t, err)
+
+	msg2, err := CreateAssignProcessMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateAssignProcessMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
