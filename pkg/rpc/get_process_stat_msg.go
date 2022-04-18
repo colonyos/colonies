5723ebc01f0b75ef8ecbfff130feb622cd8fac4a
Johan Kristiansson <kristiansson.johan@gmail.se>
2022-04-18 19:10:39 +0200

    more unittests

---
+++ pkg/rpc/get_process_stat_msg.go
@@ -0,0 +1,57 @@
+package rpc
+
+import (
+	"encoding/json"
+)
+
+const GetProcessStatPayloadType = "getprocstatmsg"
+
+type GetProcessStatMsg struct {
+	ColonyID string `json:"colonyid"`
+	MsgType  string `json:"msgtype"`
+}
+
+func CreateGetProcessStatMsg(colonyID string) *GetProcessStatMsg {
+	msg := &GetProcessStatMsg{}
+	msg.ColonyID = colonyID
+	msg.MsgType = GetProcessStatPayloadType
+
+	return msg
+}
+
+func (msg *GetProcessStatMsg) ToJSON() (string, error) {
+	jsonBytes, err := json.Marshal(msg)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonBytes), nil
+}
+
+func (msg *GetProcessStatMsg) ToJSONIndent() (string, error) {
+	jsonBytes, err := json.MarshalIndent(msg, "", "    ")
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonBytes), nil
+}
+
+func (msg *GetProcessStatMsg) Equals(msg2 *GetProcessStatMsg) bool {
+	if msg.MsgType == msg2.MsgType && msg.ColonyID == msg2.ColonyID {
+		return true
+	}
+
+	return false
+}
+
+func CreateGetProcessStatMsgFromJSON(jsonString string) (*GetProcessStatMsg, error) {
+	var msg *GetProcessStatMsg
+
+	err := json.Unmarshal([]byte(jsonString), &msg)
+	if err != nil {
+		return msg, err
+	}
+
+	return msg, nil
+}
---
+++ pkg/rpc/version_msg_test.go
@@ -0,0 +1,35 @@
+package rpc
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRPCVersionMsg(t *testing.T) {
+	msg := CreateVersionMsg("build_version", "build_type")
+	jsonString, err := msg.ToJSON()
+	assert.Nil(t, err)
+
+	msg2, err := CreateVersionMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateVersionMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
+
+func TestRPCVersionMsgIndent(t *testing.T) {
+	msg := CreateVersionMsg("build_version", "build_type")
+	jsonString, err := msg.ToJSONIndent()
+	assert.Nil(t, err)
+
+	msg2, err := CreateVersionMsgFromJSON(jsonString + "error")
+	assert.NotNil(t, err)
+
+	msg2, err = CreateVersionMsgFromJSON(jsonString)
+	assert.Nil(t, err)
+
+	assert.True(t, msg.Equals(msg2))
+}
