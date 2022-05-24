ae6b1ba97e89c563d4cd42f6a5f6691cba7855d5
Johan Kristiansson <kristiansson.johan@gmail.com>
2022-05-24 16:10:17 +0200

    adding more delay to make unit test work

--- pkg/server/colonies_server_test.go
+++ pkg/server/colonies_server_test.go
@@ -862,7 +862,7 @@ func TestMaxExecTimeMaxretries(t *testing.T) {
 	}
 
 	// Wait for the process to time out
-	time.Sleep(5 * time.Second)
+	time.Sleep(10 * time.Second)
 
 	stat, err := client.GetProcessStat(env.colonyID, env.runtimePrvKey)
 	assert.Nil(t, err)
