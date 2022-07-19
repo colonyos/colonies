302dff5026b0f202b106081e643df9039a5805a1
Johan Kristiansson <kristiansson.johan@gmail.com>
2022-07-19 15:19:35 +0200

    added lastest flag to assign cli operation

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -76,6 +76,7 @@ var EtcdClientPort int
 var EtcdPeerPort int
 var EtcdCluster []string
 var EtcdDataDir string
+var Latest bool
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
