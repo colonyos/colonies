bb238b62ab2262bef22ca867bc2b785a3c77be00
Johan Kristiansson <95801483+johankristianss@users.noreply.github.com>
2022-08-02 15:50:10 +0200

    improved coverage, also fixed generator bug (#57)

--- pkg/server/generator_handlers_security_test.go
+++ pkg/server/generator_handlers_security_test.go
@@ -1,3 +1,134 @@
 package server
 
-// TODO
+import (
+	"testing"
+
+	"github.com/colonyos/colonies/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddGeneratorSecurity(t *testing.T) {
+	env, client, server, _, done := setupTestEnv1(t)
+
+	// The setup looks like this:
+	//   runtime1 is member of colony1
+	//   runtime2 is member of colony2
+
+	generator := utils.FakeGenerator(t, env.colony1ID)
+
+	_, err := client.AddGenerator(generator, env.runtime2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.AddGenerator(generator, env.colony1PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.AddGenerator(generator, env.colony2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.AddGenerator(generator, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	server.Shutdown()
+	<-done
+}
+
+func TestGetGeneratorSecurity(t *testing.T) {
+	env, client, server, _, done := setupTestEnv1(t)
+
+	// The setup looks like this:
+	//   runtime1 is member of colony1
+	//   runtime2 is member of colony2
+
+	colonyID := env.colony1ID
+	generator := utils.FakeGenerator(t, colonyID)
+
+	_, err := client.AddGenerator(generator, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	_, err = client.GetGenerator(generator.ID, env.runtime2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerator(generator.ID, env.colony1PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerator(generator.ID, env.colony2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerator(generator.ID, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	server.Shutdown()
+	<-done
+}
+
+func TestGetGeneratorsSecurity(t *testing.T) {
+	env, client, server, _, done := setupTestEnv1(t)
+
+	// The setup looks like this:
+	//   runtime1 is member of colony1
+	//   runtime2 is member of colony2
+
+	colonyID := env.colony1ID
+	generator := utils.FakeGenerator(t, colonyID)
+
+	_, err := client.AddGenerator(generator, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	_, err = client.GetGenerators(colonyID, 100, env.runtime2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerators(colonyID, 100, env.colony1PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerators(colonyID, 100, env.colony2PrvKey)
+	assert.NotNil(t, err)
+	_, err = client.GetGenerators(colonyID, 100, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	server.Shutdown()
+	<-done
+}
+
+func TestIncGeneratorSecurity(t *testing.T) {
+	env, client, server, _, done := setupTestEnv1(t)
+
+	// The setup looks like this:
+	//   runtime1 is member of colony1
+	//   runtime2 is member of colony2
+
+	colonyID := env.colony1ID
+	generator := utils.FakeGenerator(t, colonyID)
+
+	_, err := client.AddGenerator(generator, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	err = client.IncGenerator(generator.ID, env.runtime2PrvKey)
+	assert.NotNil(t, err)
+	err = client.IncGenerator(generator.ID, env.colony1PrvKey)
+	assert.NotNil(t, err)
+	err = client.IncGenerator(generator.ID, env.colony2PrvKey)
+	assert.NotNil(t, err)
+	err = client.IncGenerator(generator.ID, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	server.Shutdown()
+	<-done
+}
+
+func TestDeleteGeneratorSecurity(t *testing.T) {
+	env, client, server, _, done := setupTestEnv1(t)
+
+	// The setup looks like this:
+	//   runtime1 is member of colony1
+	//   runtime2 is member of colony2
+
+	colonyID := env.colony1ID
+	generator := utils.FakeGenerator(t, colonyID)
+
+	_, err := client.AddGenerator(generator, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	err = client.DeleteGenerator(generator.ID, env.runtime2PrvKey)
+	assert.NotNil(t, err)
+	err = client.DeleteGenerator(generator.ID, env.colony1PrvKey)
+	assert.NotNil(t, err)
+	err = client.DeleteGenerator(generator.ID, env.colony2PrvKey)
+	assert.NotNil(t, err)
+	err = client.DeleteGenerator(generator.ID, env.runtime1PrvKey)
+	assert.Nil(t, err)
+
+	server.Shutdown()
+	<-done
+}
