30ec20b2a8f9e7cb36ec90ef63c37625dc250683
Johan Kristiansson <kristiansson.johan@gmail.se>
2022-08-29 10:29:35 +0200

    wip

--- pkg/core/code.go
+++ pkg/core/code.go
@@ -1,13 +1,13 @@
 package core
 
 type Code struct {
-	ID      string `json:"codeid"`
-	Name    string `json:"name"`
-	Package []byte `json:"package"`
-	 []byte `json:"package"`
+	ID     string `json:"codeid"`
+	Name   string `json:"name"`
+	Bundle []byte `json:"bundle"`
+	Script string `json:"script"`
 }
 
-func CreateCode(name string, package []byte) Code {
-    code := &Code{Name:name, Package:package}
-    return code
-}
+// func CreateCode(name string, package []byte) Code {
+//     code := &Code{Name:name, Package:package}
+//     return code
+// }
