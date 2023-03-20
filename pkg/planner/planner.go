dd2c22c15965932c89492718264d27f9711654aa
Johan Kristiansson <kristiansson.johan@gmail.com>
2023-03-20 18:43:30 +0100

    performance improvements

--- pkg/planner/planner.go
+++ pkg/planner/planner.go
@@ -3,6 +3,6 @@ package planner
 import "github.com/colonyos/colonies/pkg/core"
 
 type Planner interface {
-	Select(executorID string, candidates []*core.Process, latest bool) (*core.Process, error)
-	Prioritize(executorID string, candidates []*core.Process, count int, latest bool) []*core.Process
+	Select(executorID string, candidates []*core.Process) (*core.Process, error)
+	Prioritize(executorID string, candidates []*core.Process, count int) []*core.Process
 }
