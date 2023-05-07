9424e13bc136ff80da55ab37ceca08aa17d35c48
Johan Kristiansson <kristiansson.johan@gmail.se>
2023-05-07 11:01:29 +0200

    retention now working

--- pkg/server/constants.go
+++ pkg/server/constants.go
@@ -7,6 +7,5 @@ const TESTPORT = 28088
 const RELEASE_PERIOD = 1              // Period in seconds when processes are checked for max exec time or max wait time
 const GENERATOR_TRIGGER_PERIOD = 1000 // Period in milliseconds when generator is run
 const CRON_TRIGGER_PERIOD = 1000      // Period in milliseconds when cron is run
-
 const MIN_PRIORITY = -50000
 const MAX_PRIORITY = 50000
