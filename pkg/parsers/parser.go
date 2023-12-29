29cc3a2304f59be0e48485eb64e8a429a3cce284
Johan Kristiansson <kristiansson.johan@gmail.com>
2023-12-29 11:22:40 +0100

    parsers wip

---
+++ pkg/parsers/parser.go
@@ -0,0 +1,17 @@
+package parsers
+
+func ConvertCPUToInt(cpu string) int {
+	return 0
+}
+
+func ConvertMemoryToInt(mem string) int {
+	return 0
+}
+
+func ConvertCPUToString(cpu int) string {
+	return "0m"
+}
+
+func ConvertMemoryToString(mem int) string {
+	return "0Mib"
+}
