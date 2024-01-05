892b34e678aad14df1b8dc95d217c7448a039d3c
Johan Kristiansson <kristiansson.johan@gmail.se>
2024-01-05 10:39:47 +0100

    support for searching logs

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -182,12 +182,13 @@ type Database interface {
 
 	// Logging
 	AddLog(processID string, colonyName string, executorName string, timestamp int64, msg string) error
-	GetLogsByProcessID(processID string, limit int) ([]core.Log, error)
-	GetLogsByProcessIDSince(processID string, limit int, since int64) ([]core.Log, error)
-	GetLogsByExecutor(executorName string, limit int) ([]core.Log, error)
-	GetLogsByExecutorSince(executorName string, limit int, since int64) ([]core.Log, error)
+	GetLogsByProcessID(processID string, limit int) ([]*core.Log, error)
+	GetLogsByProcessIDSince(processID string, limit int, since int64) ([]*core.Log, error)
+	GetLogsByExecutor(executorName string, limit int) ([]*core.Log, error)
+	GetLogsByExecutorSince(executorName string, limit int, since int64) ([]*core.Log, error)
 	RemoveLogsByColonyName(colonyName string) error
 	CountLogs(colonyName string) (int, error)
+	SearchLogs(colonyName string, text string, days int, count int) ([]*core.Log, error)
 
 	// File management
 	AddFile(file *core.File) error
