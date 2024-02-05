d0f286cf3468ce71f85ec4e2c14292f2c007ef3a
Johan Kristiansson <kristiansson.johan@gmail.se>
2024-02-05 22:04:16 +0100

    dht

---
+++ pkg/dht/bucket.go
@@ -0,0 +1,52 @@
+package dht
+
+import (
+	"container/list"
+)
+
+type bucket struct {
+	list *list.List
+}
+
+func newBucket() *bucket {
+	bucket := &bucket{}
+	bucket.list = list.New()
+	return bucket
+}
+
+func (bucket *bucket) AddContact(contact *Contact) {
+	var element *list.Element
+	for e := bucket.list.Front(); e != nil; e = e.Next() {
+		nodeID := e.Value.(*Contact).ID
+
+		if (contact).ID.Equals(nodeID) {
+			element = e
+		}
+	}
+
+	if element == nil {
+		if bucket.list.Len() < bucketSize {
+			bucket.list.PushFront(contact)
+		} else {
+			// Check if the last contact is alive
+		}
+	} else {
+		bucket.list.MoveToFront(element)
+	}
+}
+
+func (bucket *bucket) GetContactAndCalcDistance(target *KademliaID) []*Contact {
+	var contacts []*Contact
+
+	for elt := bucket.list.Front(); elt != nil; elt = elt.Next() {
+		contact := elt.Value.(*Contact)
+		contact.CalcDistance(target)
+		contacts = append(contacts, contact)
+	}
+
+	return contacts
+}
+
+func (bucket *bucket) Len() int {
+	return bucket.list.Len()
+}
