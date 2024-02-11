5da3d5a76190000d7114ab10adf83bf5c4ef2340
Johan Kristiansson <kristiansson.johan@gmail.com>
2024-02-12 00:07:25 +0100

    libp2p support now working

--- pkg/dht/network/socket.go
+++
@@ -1,8 +0,0 @@
-package network
-
-import "context"
-
-type Socket interface {
-	Send(msg Message) error
-	Receive(context.Context) (Message, error)
-}
--- pkg/dht/rpc_header.go
+++
@@ -1,5 +0,0 @@
-package dht
-
-type RPCHeader struct {
-	Sender Contact `json:"contact"`
-}
