811adce86c93a58dc4921e49232eceaebef3f0be
Johan Kristiansson <kristiansson.johan@gmail.se>
2024-02-14 17:10:25 +0100

    rootkey must be id

--- pkg/p2p/dht/dht.go
+++ pkg/p2p/dht/dht.go
@@ -8,9 +8,10 @@ import (
 
 // DHT defines the operations supported by a Distributed Hash Table in a Kademlia network.
 type DHT interface {
+
 	// Register adds a new node to the DHT network using a bootstrap node's address and a unique KademliaID.
 	// The context allows for request cancellation and timeout control.
-	Register(bootstrapNode p2p.Node, kademliaID string, ctx context.Context) error
+	RegisterNetwork(bootstrapNode p2p.Node, kademliaID string, ctx context.Context) error
 
 	// FindContact retrieves information about a node identified by its KademliaID.
 	// It returns a Contact structure with the node's information or an error if the node is not found.
@@ -24,17 +25,21 @@ type DHT interface {
 	// '/key1/key2/.../keyN' with 1 to 5 alphanumeric sublevels and no trailing slash. The 'replicationFactor'
 	// determines the number of nodes across which the value is replicated.
 	// The first key (key1) is referred to as the root key and is used by the DHT to find the correct node to store the value.
-	Put(key string, value string, replicationFactor int, ctx context.Context) error
+	Put(id string, prvKey string, key string, value string, replicationFactor int, ctx context.Context) error
 
 	// Get retrieves all values stored under the subkeys of a given path from the DHT.
 	// The key provided must follow the defined format '/key1/key2/.../keyN' with 1 to 5 alphanumeric sublevels
 	// and no trailing slash. This method fetched data stored under the hierarchical key structure.
 	// It returns a map of subkey-value pairs if successful or an error if the retrieval operation fails or if the
 	// specified path does not exist.
-	Get(key string, replicationFactor int, ctx context.Context) ([]KV, error)
+	Get(id string, key string, replicationFactor int, ctx context.Context) ([]KV, error)
 
 	// GetContact returns the contact information of the local DHT node.
 	GetContact() Contact
+
+	RegisterNode(id string, prvKey string, node *p2p.Node, ctx context.Context) error
+
+	LookupNode(id string, name string, ctx context.Context) (*p2p.Node, error)
 
 	// Shutdown gracefully stops the DHT node and releases all resources.
 	Shutdown()
